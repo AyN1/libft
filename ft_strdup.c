6c676d42c30879f618e63d6c9fb00c77a03bc061
0 Mac Mini <johndoe@johndoe.johndoe>
2023-08-30 01:05:29 +0200

    Add len variable ft_strdup.c

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: atbicer <atbicer@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/28 23:50:35 by atbicer           #+#    #+#             */
-/*   Updated: 2023/08/29 02:48:00 by atbicer          ###   ########.fr       */
+/*   Updated: 2023/08/30 00:54:07 by atbicer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,12 @@
 char	*ft_strdup(const char *s1)
 {
 	char	*str;
+	size_t	len;
 
-	str = ft_calloc(ft_strlen(s1) + 1, sizeof(char));
+	len = ft_strlen(s1);
+	str = ft_calloc(len + 1, sizeof(char));
 	if (!str)
 		return (NULL);
-	ft_strlcpy(str, s1, ft_strlen(s1) + 1); // try with memcpy
+	ft_strlcpy(str, s1, len + 1); // try with memcpy
 	return (str);
 }
