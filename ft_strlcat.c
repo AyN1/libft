47e1e404ba19c9775c06c9e48d2eaf94f7d05186
Mac Mini <abicer.pro@gmail.com>
2023-08-30 01:32:19 +0200

    refactor ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: atbicer <atbicer@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/28 03:03:08 by atbicer           #+#    #+#             */
-/*   Updated: 2023/08/28 03:41:40 by atbicer          ###   ########.fr       */
+/*   Updated: 2023/08/30 01:30:32 by atbicer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	size_t	total_len;
 
 	dst_len = ft_strlen(dst);
-	total_len = dstsize - dst_len;
 	if (dstsize > dst_len)
 	{
+		total_len = dstsize - dst_len;
 		ft_strlcpy(dst + dst_len, src, total_len);
 		return (ft_strlen(src) + dst_len);
 	}
