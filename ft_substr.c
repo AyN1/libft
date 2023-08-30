d64c4120946d7d94502448105ebabd74053269d7
Mac Mini <abicer.pro@gmail.com>
2023-08-30 02:32:32 +0200

    Fix overflow bug. When start >= len_s ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: atbicer <atbicer@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 00:27:00 by atbicer           #+#    #+#             */
-/*   Updated: 2023/08/29 02:48:51 by atbicer          ###   ########.fr       */
+/*   Updated: 2023/08/30 02:29:39 by atbicer          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,12 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		return (NULL);
 	len_s = ft_strlen(s);
 	if (start >= len_s)
-		len = 0;
+		return (ft_strdup(""));
 	if (len > len_s - start)
 		len = len_s - start;
 	substr = ft_calloc(len + 1, sizeof(char));
 	if (!substr)
 		return (NULL);
-	// ft_strlcpy(substr, s + start, len + 1);
-	ft_memcpy(substr, s + start, len);
+	ft_strlcpy(substr, s + start, len + 1);
 	return (substr);
 }
